f6364692884ce92edad69299a57bb57e314933ad
wxuns <php7ml@gmail.com>
2021-12-06 18:01:01 +0800

    feat: 新增登录接口

--- src/Controls/UserController.php
+++ src/Controls/UserController.php
@@ -3,22 +3,32 @@
 namespace iLzx\AdminStarter\Controls;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use iLzx\AdminStarter\Facades\JWT\JWT;
+use iLzx\AdminStarter\Models\Admin;
 use iLzx\AdminStarter\Rules\Captcha;
 
 class UserController extends Controller
 {
-    public function login(Request $request)
+    public function login(Request $request): array
     {
-        $validate = $request->validate([
+        $validate = Validator::make($request->all(), [
             'username' => 'required',
             'password' => 'required',
             'redomStr' => 'required',
             'code'     => ['required', new Captcha($request->redomStr)],
         ]);
-        $errors   = $validate->all();
-        if (!$errors) {
-            dd($errors);
+        $errors   = $validate->errors()->first();
+        if ($errors) {
+            return adminOutput(201, [], $errors);
         }
+        $admin = Admin::where('username', $request->username)
+            ->where('password', $request->password)->select()->first();
+        if (!$admin) {
+            return adminOutput(202, []);
+        }
+        $jwt = JWT::createToken($request->password);
+        return adminOutput(200, ['token' => $jwt->toString()]);
     }
 
     public function getTopMenu(Request $request)
